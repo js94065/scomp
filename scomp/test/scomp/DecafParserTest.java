fdd4e91fdc04cf906a8631cbd0acb4383042d40d
codistmonk <codistmonk@0535f760-9de1-46c3-3f01-77990706043b>
2010-06-10 10:23:32 +0000

    Restarted DecafParser.cup. Added some tests to DecafParserTest.

--- scomp/test/scomp/DecafParserTest.java
+++ scomp/test/scomp/DecafParserTest.java
@@ -13,8 +13,65 @@ public class DecafParserTest {
 	
 	@Test
 	public final void testSmallestProgram() throws Exception {
-		parse("class Program{}");
+		parse(SMALLEST_PROGRAM);
 	}
+	
+	@Test
+	public final void testProgramWithAField() throws Exception {
+		parse(PROGRAM_WITH_A_FIELD);
+	}
+	
+	@Test
+	public final void testProgramWithAMethod() throws Exception {
+		parse(PROGRAM_WITH_A_METHOD);
+	}
+	
+	@Test
+	public final void testProgramWithAFieldAndAMethod() throws Exception {
+		parse(PROGRAM_WITH_A_FIELD_AND_A_METHOD);
+	}
+	
+	public static final String SMALLEST_PROGRAM =
+		"class Program{}";
+	
+	public static final String PROGRAM_WITH_A_FIELD =
+		"class Program {\n" +
+		"\n" +
+		"	int x;\n" +
+		"\n" +
+		"}";
+	
+	public static final String PROGRAM_WITH_A_METHOD =
+		"class Program {\n" +
+		"\n" +
+		"	void f() {\n" +
+		"	}\n" +
+		"\n" +
+		"}";
+	
+	public static final String PROGRAM_WITH_A_FIELD_AND_A_METHOD =
+		"class Program {\n" +
+		"\n" +
+		"	int x;\n" +
+		"\n" +
+		"	void f() {\n" +
+		"	}\n" +
+		"\n" +
+		"}";
+	
+	public static final String PROGRAM_WITH_FIELDS_AND_METHODS =
+		"class Program {\n" +
+		"\n" +
+		"	int x, y;\n" +
+		"\n" +
+		"	void f() {\n" +
+		"	}\n" +
+		"\n" +
+		// TODO change following method into function as soon as parser can handle it
+		"	void g() {\n" +
+		"	}\n" +
+		"\n" +
+		"}";
 	
 	/**
 	 * 
