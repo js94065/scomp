380dd12cb43a6d49e5c11d13d65895a3835443b9
js94065@gmail.com <js94065@gmail.com@0535f760-9de1-46c3-3f01-77990706043b>
2010-07-24 07:34:20 +0000

    Replacing the use of import package.* with import of actual classes used.

--- trunk/scomp/test/scomp/DecafParserTest.java
+++ trunk/scomp/test/scomp/DecafParserTest.java
@@ -1,10 +1,11 @@
 package scomp;
 
-import static org.junit.Assert.*;
-
-import java_cup.runtime.Symbol;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import java_cup.runtime.Symbol;
 
 /**
  * 
