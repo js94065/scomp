b8144de3abb455cc12b76305a3850922eb9e0d89
luuwilson <luuwilson@0535f760-9de1-46c3-3f01-77990706043b>
2010-08-17 06:50:17 +0000

    Implemented test Rule 10 and updated Rule 10 in SemanticRules

--- trunk/scomp/src/scomp/semantics/SemanticRules.java
+++ trunk/scomp/src/scomp/semantics/SemanticRules.java
@@ -431,15 +431,30 @@ public final class SemanticRules implements Visitor {
 	 * <br> not null
 	 */
 	private final void checkRule10(ArrayLocation location) {
-//		if(!this.getCurrentScope().get(location.getIdentifier()).getType().equals(ArrayLocation.class)) {
-//			this.logError(location.getTokenRow(), location.getTokenColumn(),
-//					"The variable " + location.getIdentifier() + " is not an array type");
-//		}
-//		
-//		if(!location.getOffset().getType().equals(int.class)) {
-//			this.logError(location.getTokenRow(), location.getTokenColumn(),
-//					"The array offset is not an int type for array variable " + location.getIdentifier());
-//		}
+
+		if(this.getCurrentScope().containsKey(location.getIdentifier())) {
+			
+			if(!this.getCurrentScope().get(location.getIdentifier()).getClass().equals(ArrayFieldDeclaration.class)) {
+				this.logError(location.getTokenRow(), location.getTokenColumn(),
+						"The variable " + location.getIdentifier() + " is not an array type");
+			}
+			
+			/* TODO temporary solution when getOffset() returns a MethodCallExpression.getType() == null object
+			 */ 
+			if(location.getOffset().getClass().equals(MethodCallExpression.class)) {
+				String methodName = ((MethodCallExpression)location.getOffset()).getMethodCall().getMethodName();
+				if(!this.getCurrentScope().get(methodName).getType().equals(int.class)) {
+					this.logError(location.getTokenRow(), location.getTokenColumn(),
+						"The array offset is not an int type");
+				}
+			}
+			else {
+				if(!location.getOffset().getType().equals(int.class)) {
+					this.logError(location.getTokenRow(), location.getTokenColumn(),
+						"The array offset is not an int type");
+				}
+			}
+		}
 	}
 	
 	
