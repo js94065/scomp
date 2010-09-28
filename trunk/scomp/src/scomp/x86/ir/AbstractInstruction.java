241c078dfc73833a4d22ebd75346bdac4cd0d2b7
codistmonk <codistmonk@0535f760-9de1-46c3-3f01-77990706043b>
2010-09-28 23:11:27 +0000

    Added x86 IR classes and updated translation classes to make the test "callout" pass (Mac OS X).

--- trunk/scomp/src/scomp/x86/ir/AbstractInstruction.java
+++ trunk/scomp/src/scomp/x86/ir/AbstractInstruction.java
@@ -1,5 +1,13 @@
 package scomp.x86.ir;
 
+import static scomp.x86.ir.Register.Name.EAX;
+import static scomp.x86.ir.Register.Name.EBP;
+import static scomp.x86.ir.Register.Name.ECX;
+import static scomp.x86.ir.Register.Name.EIP;
+import static scomp.x86.ir.Register.Name.ESP;
+
+import scomp.x86.ir.Register.Name;
+
 /**
  * This is the base class for x86 instructions.
  * 
@@ -44,5 +52,36 @@ public abstract class AbstractInstruction extends AbstractProgramElement {
 	 * {@value}.
 	 */
 	public static final String SIZE_SUFFIX_64 = "q";
+	
+	/**
+	 * 
+	 * @param name
+	 * <br>Not null
+	 * @param size
+	 * <br>Not null
+	 * <br>Range: { {@link AbstractInstruction#SIZE_SUFFIX_32}, {@link AbstractInstruction#SIZE_SUFFIX_64} }
+	 * @return
+	 * <br>Not null
+	 */
+	public static final Name getResizedName(final Name name, final String size) {
+		if (SIZE_SUFFIX_32.equals(size)) {
+			switch (name) {
+			case RAX:
+				return EAX;
+			case RCX:
+				return ECX;
+			case RSP:
+				return ESP;
+			case RBP:
+				return EBP;
+			case RIP:
+				return EIP;
+			default:
+				break;
+			}
+		}
+		
+		return name;
+	}
 	
 }
---
+++ trunk/scomp/src/scomp/x86/ir/Lea.java
@@ -0,0 +1,27 @@
+package scomp.x86.ir;
+
+/**
+ * This class represents the x86 "lea" instruction.
+ * 
+ * @author codistmonk (creation 2010-09-28)
+ *
+ */
+public final class Lea extends AbstractBinaryInstruction {
+	
+	/**
+	 * 
+	 * @param sizeSuffix
+	 * <br>Not null
+	 * <br>Shared
+	 * @param firstOperand
+	 * <br>Not null
+	 * <br>Shared
+	 * @param secondOperand
+	 * <br>Not null
+	 * <br>Shared
+	 */
+	public Lea(final String sizeSuffix, final AbstractOperand firstOperand, final AbstractOperand secondOperand) {
+		super(sizeSuffix, firstOperand, secondOperand);
+	}
+	
+}
