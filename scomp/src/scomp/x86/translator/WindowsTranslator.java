7abff87af89f8a925287d55ab4309edcb5c84570
js94065 <js94065@0535f760-9de1-46c3-3f01-77990706043b>
2013-03-03 19:34:43 +0000

    Adding windows translation.

--- scomp/src/scomp/x86/translator/WindowsTranslator.java
+++ scomp/src/scomp/x86/translator/WindowsTranslator.java
@@ -1,32 +1,48 @@
 package scomp.x86.translator;
 
+import static scomp.x86.ir.Register.Name.*;
 import scomp.ir.MethodCallout;
 import scomp.x86.ir.AbstractInstruction;
+import scomp.x86.ir.Add;
+import scomp.x86.ir.Call;
+import scomp.x86.ir.CompositeIntegerValue;
+import scomp.x86.ir.Label;
+import scomp.x86.ir.LabelOperand;
+import scomp.x86.ir.LabelRelativeAddress;
+import scomp.x86.ir.Mov;
+import scomp.x86.ir.Push;
+import scomp.x86.ir.Register;
 import scomp.x86.ir.Register.Name;
 
 /**
  * This is the Windows implementation of the Decaf -&gt; x86 translator.
  * 
- * @author codistmonk (creation 2010-09-26)
+ * @author codistmonk, js94065 (creation 2010-09-26)
  *
  */
 public final class WindowsTranslator extends AbstractTranslator {
 	
 	@Override
 	protected final void x86MOV(final String labelName, final Name registerName) {
-		// TODO Auto-generated method stub
-		
+		this.getProcedureSection().add(new Mov(this.getDefaultSizeSuffix(),
+				new LabelOperand("$"+labelName),
+				new Register(this.getResizedName(registerName))));
 	}
 	
 	@Override
 	protected final void x86PUSH(final String labelName) {
-		// TODO Auto-generated method stub
-		
+		this.x86MOV(labelName, EAX);
+		this.x86PUSH(EAX);
 	}
 	
 	@Override
 	protected final void afterChildren(final MethodCallout methodCallout) {
-		// TODO Auto-generated method stub
+		final int argumentCount = methodCallout.getArguments().size();
+		final String callName = "_" + methodCallout.getMethodName();
+		
+		this.getProcedureSection().add(new Call(this.getDefaultSizeSuffix(), new LabelOperand(callName)));
+		this.getProcedureSection().add(new Add(this.getDefaultSizeSuffix(),
+				new CompositeIntegerValue(this.getDefaultVariableByteCount(), argumentCount), new Register(this.getResizedName(RSP))));
 		
 	}
 	
